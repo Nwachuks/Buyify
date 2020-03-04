ee51f65bffee784e84c7a76e93f9efb6961cbf31
Nwachukwu Ejiofor <nwachukwu.ejiofor@xputertechnologies.com>
2020-03-04 16:06:12 +0100

    Create page model

---
+++ Buyify.API/models/page.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+// Page Schema
+const PageSchema = mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    slug: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    sorting: {
+        type: String,
+        required: true
+    }
+})
+
+const Page = module.exports = mongoose.model('Page', PageSchema);
